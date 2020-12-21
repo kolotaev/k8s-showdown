ad5431e4f0cfe19cf80a76126f0593f40b389b14
kolotaev <kolotaev@tut.by>
2020-12-21 15:14:41 +0300

    Use 80 port as the default within cluster

--- services/catalog/server.rb
+++ services/catalog/server.rb
@@ -12,13 +12,14 @@ before '/' do
 end
 
 get '/' do
-  res1 = Net::HTTP.get('http://greet.showdown', '/')
-  res2 = Net::HTTP.get('http://time.showdown', '/')
+  res1 = Net::HTTP.get(ENV['GREET_URL'], "/?name=#{SecureRandom.alphanumeric}")
+  res2 = Net::HTTP.get(ENV['TIME_URL'], '/')
 
   data = {
     'name': 'catalog server',
-    'greeting': JSON.parse(res1.body)['greeting'],
-    'timestamp': JSON.parse(res2.body)['ts'],
+    'greeting': JSON.parse(res1)['greeting'],
+    'ts': JSON.parse(res2)['timestamp'],
+    'aggregate': true,
   }
   data.to_json
 end
